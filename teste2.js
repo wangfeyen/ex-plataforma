69436c1d4eaf7979947820dc283ab872e63f4015
wangfeyen <wangfeyen@gmail.com>
2023-08-08 11:04:17 -0300

    Update teste2.js

--- teste2.js
+++ teste2.js
@@ -4,16 +4,27 @@ const pessoas = prompt("Cabe apenas uma pessoa?");
 const pesado = prompt("É pesado?");
 const pedal = prompt("Tem pedal?");
 const capacete = prompt("Usa capacete?");
-const respostas = [terrestre,pessoas,pesado,pedal,capacete]
-const mensagem =""
+const respostas = terrestre+pessoas+pesado+pedal+capacete
+
+let mensagem =""
+let falha=false;
 switch (respostas) {
-    case ["s","s","s","n","n"]:
+    case ("sssnn"):
         mensagem="Trator";
+        break;
+    
+    case("ssnss"):
+        mensagem="Bicicleta";
+        break;
+    case("snsns"):
+        mensagem="Moto";
         break;
 
     default:
-        console.log("n deu...")
+        console.log("Transporte indefinido!")
+        falha=true;
         break;
 }
-console.log("a mensagem é: ",mensagem)
-console.log(respostas);
+if (falha===false){
+    console.log("O transporte é",mensagem,"!")
+}
